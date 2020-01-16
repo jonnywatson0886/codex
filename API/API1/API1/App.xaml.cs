ba49488c655ac08b746ccda441da9f05cd559fd1
jonny watson <jonnywatson0886@gmail.com>
2020-01-16 15:22:54 +0000

    added in first commit with tutorial api in process

---
+++ API/API1/API1/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace API1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        APIHelper
+    }
+}
---
+++ API/API1/DemoLibrary/APIHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public static class APIHelper
+    {
+        //made static to be once per appliaction can also be checked for null
+        public static HttpClient APIClient { get; set; }
+        /// <summary>
+        /// setup for the API client
+        /// </summary>
+        public static void NewAPIClient()
+        {
+             //intislise client 
+            APIClient = new HttpClient();
+            //would usually add in for single address
+            //APIClient.BaseAddress = new Uri("http://webaddress");
+            //end of usual
+            APIClient.DefaultRequestHeaders.Accept.Clear();
+            APIClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
